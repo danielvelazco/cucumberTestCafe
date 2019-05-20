5c5311d7d4b9d25af343a0f9b36a4a97883f2101
Daniel Velazco <danvelazco27@gmail.com>
2019-05-20 01:04:53 -0400

    first commit

---
+++ features/step_definitions/tottus_steps.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+const { Selector } = require('testcafe');
+
+
+
+Given('Navego a la pagina de google.com', async function() {
+    await testController.navigateTo('http://www.google.com');
+});
+
+When('Busco {string}', async function(terminosDeBusqueda) {
+    const campoDeBusquda = Selector('input').withAttribute('name', 'q').with({ boundTestRun: testController });
+
+    await testController.typeText(campoDeBusquda, terminosDeBusqueda)
+        .pressKey('enter');
+});
+
+When('Selecciono tottus nataniel', async function() {
+    const linkTottusNataniel = Selector('b').withText('nataniel').with({ boundTestRun: testController });
+
+    await testController.click(linkTottusNataniel);
+});
+
+When('Hago click en el boton sitio web', async function() {
+    const botonSitioWeb = Selector('a').withText('Sitio web').with({ boundTestRun: testController });
+
+    await testController.click(botonSitioWeb);
+});
+
+When('Hago click en recetas y mas', async function() {
+    const imgRecetas = Selector('#new_tottus_nav_cliente')
+        .child('ul')
+        .nth(2)
+        .with({ boundTestRun: testController });
+
+    await testController.click(imgRecetas);
+});
---
+++ features/tottus.feature
@@ -0,0 +1,12 @@
+Feature: Consulta de sucursales
+
+
+    Buscar sucursal Tottus nataniel y validar receta
+
+
+    Scenario: Validar receta de rollitos de canela
+        Given Navego a la pagina de google.com
+        When Busco "tottus sucursales"
+        When Selecciono tottus nataniel
+        When Hago click en el boton sitio web
+        When Hago click en recetas y mas 
